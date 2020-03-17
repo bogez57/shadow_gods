f3993cb9f60d4d10ff05e8b9a505a84db0014390
Jason <boagz4@yahoo.com>
2020-03-17 13:55:48 -0400

    Added debug_array class

---
+++ source/debug_array.h
@@ -0,0 +1,26 @@
+#ifndef DEBUG_ARRAY_H
+#define DEBUG_ARRAY_H
+
+template <typename Type, i64 capacity = 1>
+class DbgArray
+{
+public:
+    Type& operator[](i64 index)
+    {
+        BGZ_ASSERT(index < capacity, "Attempting to access index %i which is out of current arround bounds - current max index allowed: %i", index, capacity - 1);
+        BGZ_ASSERT(index < length, "Attempting to access index %i which hasn't been initialized yet. Current buffer length is %i", index, length);
+
+        return this->elements[index];
+    };
+
+    inline Type& Push()
+    {
+        BGZ_ASSERT(length < capacity, "Attempting to access index %i which out of current arround bounds - current max index allowed: %i", length, capacity - 1);
+        return (Type&)this->elements[length++];
+    };
+
+    i64 length {};
+    Type elements[capacity ? capacity : 1];
+};
+
+#endif //DEBUG_ARRAY_H
