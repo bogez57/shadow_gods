c4c978a7e569b62fed92b56037b4e4b83a428119
Jason <boagz4@yahoo.com>
2020-05-25 09:12:54 -0400

    Modified type library for easier vector/matrix stuff

--- source/atomic_types.h
+++ source/atomic_types.h
@@ -35,112 +35,183 @@ typedef double f64;
 
 #define Align16(Value) ((Value + 15) & ~15)
 
-struct v2f
+//Should I just template things??
+typedef union v2
 {
-    v2f() = default;
-    v2f(f32 x, f32 y);
+#ifdef __cplusplus
+    v2() = default;
+    v2 (f32 x, f32 y) : x(x), y(y){};
+#endif
     
-    union
+    struct
     {
-        f32 Elem[2];
-        struct
-        {
-            f32 x, y;
-        };
-        struct
-        {
-            f32 u, v;
-        };
-        struct
-        {
-            f32 width, height;
-        };
+        f32 x, y;
     };
-};
-
-struct v3f
-{
-    v3f() = default;
-    v3f(f32 x, f32 y, f32 z);
     
-    union
+    struct
     {
-        f32 Elem[3];
-        struct
-        {
-            f32 x, y, z;
-        };
-        
-        struct
-        {
-            f32 r, g, b;
-        };
-        
-        struct
-        {
-            v2f xy;
-            f32 _Ignored0;
-        };
-        struct
-        {
-            f32 _Ignored0;
-            v2f yz;
-        };
+        f32 u, v;
     };
-};
-
-struct v4f
-{
-    v4f() = default;
-    v4f(f32 x, f32 y, f32 z, f32 w);
     
-    union
+    struct
     {
-        f32 Elem[4];
-        struct
+        f32 width, height;
+    };
+    
+    f32 elem[2];
+    
+#ifdef __cplusplus
+    inline f32 &operator[](const int &index)
+    {
+        return elem[index];
+    }
+#endif
+} v2;
+
+typedef union v3
+{
+#ifdef __cplusplus
+    v3() = default;
+    v3 (f32 x, f32 y, f32 z) : x(x), y(y), z(z){};
+    v3 (v2 vec2, f32 z) : xy(vec2), z(z){};
+#endif
+    
+    struct
+    {
+        f32 x, y, z;
+    };
+    
+    struct
+    {
+        f32 u, v, w;
+    };
+    
+    struct
+    {
+        f32 r, g, b;
+    };
+    
+    struct
+    {
+        v2 xy;
+        f32 ignored0_;
+    };
+    
+    struct
+    {
+        f32 ignored1_;
+        v2 yz;
+    };
+    
+    struct
+    {
+        v2 uv;
+        f32 ignored2_;
+    };
+    
+    struct
+    {
+        f32 ignored3_;
+        v2 vw;
+    };
+    
+    f32 elem[3];
+    
+#ifdef __cplusplus
+    inline f32 &operator[](const int &Index)
+    {
+        return elem[Index];
+    }
+#endif
+} v3;
+
+typedef union v4
+{
+#ifdef __cplusplus
+    v4() = default;
+    v4(v2 a, v2 b) : xy(a), zw(b){};
+    v4(v3 a, f32 w) : xyz(a), w(w){};
+#endif
+    struct
+    {
+        union
         {
-            union
+            v3 xyz;
+            struct
             {
-                v3f rgb;
-                struct
-                {
-                    f32 r, g, b;
-                };
+                f32 x, y, z;
             };
-            
-            f32 a;
         };
         
-        struct
-        {
-            union
-            {
-                v3f xyz;
-                struct
-                {
-                    f32 x, y, z;
-                };
-            };
-            
-            f32 w;
-        };
-        
-        struct
-        {
-            v2f xy;
-            f32 _Ignored0;
-            f32 _Ignored1;
-        };
-        
-        struct
-        {
-            f32 _Ignored0;
-            v2f yz;
-            f32 _Ignored1;
-        };
+        f32 w;
     };
+    
+    struct
+    {
+        union
+        {
+            v3 rgb;
+            struct
+            {
+                f32 r, g, b;
+            };
+        };
+        
+        f32 a;
+    };
+    
+    struct
+    {
+        v2 xy;
+        f32 ignored0_;
+        f32 ignored1_;
+    };
+    
+    struct
+    {
+        f32 ignored2_;
+        v2 yz;
+        f32 ignored3_;
+    };
+    
+    struct
+    {
+        f32 ignored4_;
+        f32 ignored5_;
+        v2 zw;
+    };
+    
+    f32 elem[4];
+    
+#ifdef __cplusplus
+    inline f32 &operator[](const int &index)
+    {
+        return elem[index];
+    }
+#endif
+} v4;
+
+struct mat4x4
+{
+    //These are stored ROW MAJOR - elem[ROW][COLUMN]!!!
+    f32 elem[4][4];
 };
 
+struct mat3x3
+{
+    //These are stored ROW MAJOR - elem[ROW][COLUMN]!!!
+    f32 elem[3][3];
+};
+
+struct mat2x2
+{
+    //These are stored ROW MAJOR - elem[ROW][COLUMN]!!!
+    f32 elem[2][2];
+};
+
+inline mat4x4 IdentityMatrix();
+
+//Other v2's I might use. Torn on whether or not I should template things but I think for 90 percent of what I'm using vectors for floats should be what I want
 struct v2i
 {
     v2i() = default;
@@ -224,28 +295,15 @@ struct v4ui32
     };
 };
 
-struct mat4x4
-{
-    //These are stored ROW MAJOR - elem[ROW][COLUMN]!!!
-    f32 elem[4][4];
-};
-
-struct mat2x2
-{
-    //These are stored ROW MAJOR - elem[ROW][COLUMN]!!!
-    f32 elem[2][2];
-};
-
-inline mat4x4 IdentityMatrix();
-local_func v2f CastV2IToV2F(v2i vecToCast);
+local_func v2 CastV2IToV2F(v2i vecToCast);
 
 #endif
 
 #ifdef ATOMIC_TYPES_IMPL
 
-v2f CastV2IToV2F(v2i vecToCast)
+v2 CastV2IToV2F(v2i vecToCast)
 {
-    v2f result{};
+    v2 result{};
     
     result.x = (f32)vecToCast.x;
     result.y = (f32)vecToCast.y;
@@ -253,7 +311,7 @@ v2f CastV2IToV2F(v2i vecToCast)
     return result;
 }
 
-v2i CastV2FToV2I(v2f vecToCast)
+v2i CastV2FToV2I(v2 vecToCast)
 {
     v2i result{};
     
@@ -263,13 +321,8 @@ v2i CastV2FToV2I(v2f vecToCast)
     return result;
 };
 
-v2f::v2f(f32 x, f32 y)
-: x(x)
-, y(y)
-{}
-
 inline b
-operator==(v2f a, v2f B)
+operator==(v2 a, v2 B)
 {
     b result { false };
     
@@ -280,7 +333,7 @@ operator==(v2f a, v2f B)
 };
 
 inline b
-operator!=(v2f a, v2f B)
+operator!=(v2 a, v2 B)
 {
     b result { false };
     
@@ -290,10 +343,10 @@ operator!=(v2f a, v2f B)
     return result;
 };
 
-inline v2f
-operator*(f32 a, v2f B)
+inline v2
+operator*(f32 a, v2 B)
 {
-    v2f result;
+    v2 result;
     
     result.x = a * B.x;
     result.y = a * B.y;
@@ -301,36 +354,36 @@ operator*(f32 a, v2f B)
     return (result);
 }
 
-inline v2f
-operator*(v2f B, f32 a)
+inline v2
+operator*(v2 B, f32 a)
 {
-    v2f result = a * B;
+    v2 result = a * B;
     
     return (result);
 }
 
-inline v2f&
-operator*=(v2f& B, f32 a)
+inline v2&
+operator*=(v2& B, f32 a)
 {
     B = a * B;
     
     return (B);
 }
 
-inline v2f
-operator/(v2f b, f32 a)
+inline v2
+operator/(v2 b, f32 a)
 {
-    v2f result;
+    v2 result;
     result.x = b.x / a;
     result.y = b.y / a;
     
     return result;
 }
 
-inline v2f
-operator+(v2f a, v2f B)
+inline v2
+operator+(v2 a, v2 B)
 {
-    v2f result;
+    v2 result;
     
     result.x = a.x + B.x;
     result.y = a.y + B.y;
@@ -338,10 +391,10 @@ operator+(v2f a, v2f B)
     return (result);
 }
 
-inline v2f
-operator+(v2f a, f32 B)
+inline v2
+operator+(v2 a, f32 B)
 {
-    v2f result;
+    v2 result;
     
     result.x = a.x + B;
     result.y = a.y + B;
@@ -349,16 +402,16 @@ operator+(v2f a, f32 B)
     return (result);
 }
 
-inline v2f&
-operator+=(v2f& a, v2f B)
+inline v2&
+operator+=(v2& a, v2 B)
 {
     a = a + B;
     
     return (a);
 }
 
-inline v2f&
-operator+=(v2f& a, f32 B)
+inline v2&
+operator+=(v2& a, f32 B)
 {
     a.x = a.x + B;
     a.y = a.y + B;
@@ -366,8 +419,8 @@ operator+=(v2f& a, f32 B)
     return (a);
 }
 
-inline v2f&
-operator-=(v2f& a, f32 B)
+inline v2&
+operator-=(v2& a, f32 B)
 {
     a.x = a.x - B;
     a.y = a.y - B;
@@ -375,8 +428,8 @@ operator-=(v2f& a, f32 B)
     return (a);
 }
 
-inline v2f&
-operator-=(v2f& a, v2f B)
+inline v2&
+operator-=(v2& a, v2 B)
 {
     a.x = a.x - B.x;
     a.y = a.y - B.y;
@@ -384,10 +437,10 @@ operator-=(v2f& a, v2f B)
     return (a);
 }
 
-inline v2f
-operator-(v2f a, v2f B)
+inline v2
+operator-(v2 a, v2 B)
 {
-    v2f result;
+    v2 result;
     
     result.x = a.x - B.x;
     result.y = a.y - B.y;
@@ -395,10 +448,10 @@ operator-(v2f a, v2f B)
     return (result);
 }
 
-inline v2f
-operator-(v2f a, f32 b)
+inline v2
+operator-(v2 a, f32 b)
 {
-    v2f result;
+    v2 result;
     
     result.x = a.x - b;
     result.y = a.y - b;
@@ -406,10 +459,10 @@ operator-(v2f a, f32 b)
     return (result);
 }
 
-inline v2f
-operator-(v2f a)
+inline v2
+operator-(v2 a)
 {
-    v2f result;
+    v2 result;
     
     result.x = -a.x;
     result.y = -a.y;
@@ -417,16 +470,10 @@ operator-(v2f a)
     return(result);
 }
 
-v3f::v3f(f32 x, f32 y, f32 z)
-: x(x)
-, y(y)
-, z(z)
-{}
-
-inline v3f
-operator*(f32 A, v3f B)
+inline v3
+operator*(f32 A, v3 B)
 {
-    v3f result;
+    v3 result;
     
     result.x = A*B.x;
     result.y = A*B.y;
@@ -435,26 +482,26 @@ operator*(f32 A, v3f B)
     return(result);
 }
 
-inline v3f
-operator*(v3f B, f32 A)
+inline v3
+operator*(v3 B, f32 A)
 {
-    v3f result = A*B;
+    v3 result = A*B;
     
     return(result);
 }
 
-inline v3f &
-operator*=(v3f &B, f32 A)
+inline v3 &
+operator*=(v3 &B, f32 A)
 {
     B = A * B;
     
     return(B);
 }
 
-inline v3f
-operator-(v3f A)
+inline v3
+operator-(v3 A)
 {
-    v3f result;
+    v3 result;
     
     result.x = -A.x;
     result.y = -A.y;
@@ -463,10 +510,10 @@ operator-(v3f A)
     return(result);
 }
 
-inline v3f
-operator+(v3f A, v3f B)
+inline v3
+operator+(v3 A, v3 B)
 {
-    v3f result;
+    v3 result;
     
     result.x = A.x + B.x;
     result.y = A.y + B.y;
@@ -475,10 +522,10 @@ operator+(v3f A, v3f B)
     return(result);
 }
 
-inline v3f
-operator+(v3f a, f32 B)
+inline v3
+operator+(v3 a, f32 B)
 {
-    v3f result;
+    v3 result;
     
     result.x = a.x + B;
     result.y = a.y + B;
@@ -487,18 +534,18 @@ operator+(v3f a, f32 B)
     return (result);
 }
 
-inline v3f &
-operator+=(v3f &A, v3f B)
+inline v3 &
+operator+=(v3 &A, v3 B)
 {
     A = A + B;
     
     return(A);
 }
 
-inline v3f
-operator-(v3f A, v3f B)
+inline v3
+operator-(v3 A, v3 B)
 {
-    v3f result;
+    v3 result;
     
     result.x = A.x - B.x;
     result.y = A.y - B.y;
@@ -507,25 +554,18 @@ operator-(v3f A, v3f B)
     return(result);
 }
 
-inline v3f &
-operator-=(v3f &A, v3f B)
+inline v3 &
+operator-=(v3 &A, v3 B)
 {
     A = A - B;
     
     return(A);
 }
 
-v4f::v4f(f32 x, f32 y, f32 z, f32 w)
-: x(x)
-, y(y)
-, z(z)
-, w(w)
-{}
-
-inline v4f
-operator*(f32 a, v4f B)
+inline v4
+operator*(f32 a, v4 B)
 {
-    v4f result;
+    v4 result;
     
     result.x = a*B.x;
     result.y = a*B.y;
@@ -535,26 +575,26 @@ operator*(f32 a, v4f B)
     return(result);
 }
 
-inline v4f
-operator*(v4f B, f32 a)
+inline v4
+operator*(v4 B, f32 a)
 {
-    v4f result = a*B;
+    v4 result = a*B;
     
     return(result);
 }
 
-inline v4f &
-operator*=(v4f &B, f32 a)
+inline v4 &
+operator*=(v4 &B, f32 a)
 {
     B = a * B;
     
     return(B);
 }
 
-inline v4f
-operator+(v4f a, v4f B)
+inline v4
+operator+(v4 a, v4 B)
 {
-    v4f result;
+    v4 result;
     
     result.x = a.x + B.x;
     result.y = a.y + B.y;
@@ -564,18 +604,18 @@ operator+(v4f a, v4f B)
     return(result);
 }
 
-inline v4f &
-operator+=(v4f &a, v4f B)
+inline v4 &
+operator+=(v4 &a, v4 B)
 {
     a = a + B;
     
     return(a);
 }
 
-inline v4f
-operator-(v4f a, v4f b)
+inline v4
+operator-(v4 a, v4 b)
 {
-    v4f result;
+    v4 result;
     
     result.x = a.x - b.x;
     result.y = a.y - b.y;
@@ -585,10 +625,10 @@ operator-(v4f a, v4f b)
     return (result);
 }
 
-inline v4f
-operator-(v4f a)
+inline v4
+operator-(v4 a)
 {
-    v4f result;
+    v4 result;
     
     result.x = -a.x;
     result.y = -a.y;
@@ -598,6 +638,151 @@ operator-(v4f a)
     return (result);
 }
 
+inline mat4x4 IdentityMatrix()
+{
+    mat4x4 R =
+    {
+        {{1, 0, 0, 0},
+            {0, 1, 0, 0},
+            {0, 0, 1, 0},
+            {0, 0, 0, 1}},
+    };
+    
+    return(R);
+}
+
+local_func v4
+TransformVec(mat4x4 A, v4 P)
+{
+    v4 R;
+    
+    R.x = P.x*A.elem[0][0] + P.y*A.elem[0][1] + P.z*A.elem[0][2] + P.w*A.elem[0][3];
+    R.y = P.x*A.elem[1][0] + P.y*A.elem[1][1] + P.z*A.elem[1][2] + P.w*A.elem[1][3];
+    R.z = P.x*A.elem[2][0] + P.y*A.elem[2][1] + P.z*A.elem[2][2] + P.w*A.elem[2][3];
+    R.w = P.x*A.elem[3][0] + P.y*A.elem[3][1] + P.z*A.elem[3][2] + P.w*A.elem[3][3];
+    
+    return(R);
+};
+
+inline mat4x4
+Transpose(mat4x4 A)
+{
+    mat4x4 R;
+    
+    for(int j = 0; j <= 3; ++j)
+    {
+        for(int i = 0; i <= 3; ++i)
+        {
+            R.elem[j][i] = A.elem[i][j];
+        }
+    }
+    
+    return(R);
+}
+
+inline v4
+operator*(mat4x4 A, v4 P)
+{
+    v4 R = TransformVec(A, P);
+    return(R);
+};
+
+local_func mat4x4
+operator*(mat4x4 A, mat4x4 B)
+{
+    // NOTE(casey): This is written to be instructive, not optimal!
+    
+    mat4x4 R = {};
+    
+    for(int r = 0; r <= 3; ++r) // NOTE(casey): Rows (of A)
+    {
+        for(int c = 0; c <= 3; ++c) // NOTE(casey): Column (of B)
+        {
+            for(int i = 0; i <= 3; ++i) // NOTE(casey): Columns of A, rows of B!
+            {
+                R.elem[r][c] += A.elem[r][i]*B.elem[i][c];
+            }
+        }
+    }
+    
+    return(R);
+}
+
+inline mat4x4
+PerspectiveProjection(f32 aspectRatio, f32 focalLength)
+{
+    f32 a = 1.0f;
+    f32 b = aspectRatio;
+    f32 c = focalLength;
+    
+    mat4x4 r =
+    {
+        {
+            {a*c, 0, 0, 0},
+            {0, b*c, 0, 0},
+            {0,   0, 1, 0},
+            {0,   0,-1, 0}
+        }
+    };
+    
+    return r;
+};
+
+local_func mat4x4
+ColumnPicture3x3(v3 xAxis, v3 yAxis, v3 zAxis)
+{
+    mat4x4 result =
+    {
+        {
+            {xAxis.x, yAxis.x, zAxis.x, 0.0f},
+            {xAxis.y, yAxis.y, zAxis.y, 0.0f},
+            {xAxis.z, yAxis.z, zAxis.z, 0.0f},
+            {   0.0f,    0.0f,    0.0f, 1.0f}
+        }
+    };
+    
+    return result;
+};
+
+local_func mat4x4
+RowPicture3x3(v3 xAxis, v3 yAxis, v3 zAxis)
+{
+    mat4x4 result =
+    {
+        {
+            {xAxis.x, xAxis.y, xAxis.z, 0.0f},
+            {yAxis.x, yAxis.y, yAxis.z, 0.0f},
+            {zAxis.x, zAxis.y, zAxis.z, 0.0f},
+            {   0.0f,    0.0f,    0.0f, 1.0f}
+        }
+    };
+    
+    return result;
+};
+
+local_func mat4x4
+Translate(mat4x4 A, v4 T)
+{
+    mat4x4 result = A;
+    
+    result.elem[0][3] += T.x;
+    result.elem[1][3] += T.y;
+    result.elem[2][3] += T.z;
+    
+    return result;
+};
+
+local_func mat4x4
+CamTransform(v3 xAxis, v3 yAxis, v3 zAxis, v3 vecToTransform)
+{
+    mat4x4 result = RowPicture3x3(xAxis, yAxis, zAxis);
+    v4 vecToTransform_4d {vecToTransform, 1.0f};
+    result = Translate(result, -(result*vecToTransform_4d));
+    
+    return result;
+};
+
+//Other v2 implementations
 v2i::v2i(i32 x, i32 y)
 : x(x)
 , y(y)
@@ -701,149 +886,5 @@ v4i::v4i(i32 x, i32 y, i32 z, i32 w)
 , z(z)
 , w(w)
 {}
-
-inline mat4x4 IdentityMatrix()
-{
-    mat4x4 R =
-    {
-        {{1, 0, 0, 0},
-            {0, 1, 0, 0},
-            {0, 0, 1, 0},
-            {0, 0, 0, 1}},
-    };
-    
-    return(R);
-}
-
-local_func v4f
-TransformVec(mat4x4 A, v4f P)
-{
-    v4f R;
-    
-    R.x = P.x*A.elem[0][0] + P.y*A.elem[0][1] + P.z*A.elem[0][2] + P.w*A.elem[0][3];
-    R.y = P.x*A.elem[1][0] + P.y*A.elem[1][1] + P.z*A.elem[1][2] + P.w*A.elem[1][3];
-    R.z = P.x*A.elem[2][0] + P.y*A.elem[2][1] + P.z*A.elem[2][2] + P.w*A.elem[2][3];
-    R.w = P.x*A.elem[3][0] + P.y*A.elem[3][1] + P.z*A.elem[3][2] + P.w*A.elem[3][3];
-    
-    return(R);
-};
-
-inline mat4x4
-Transpose(mat4x4 A)
-{
-    mat4x4 R;
-    
-    for(int j = 0; j <= 3; ++j)
-    {
-        for(int i = 0; i <= 3; ++i)
-        {
-            R.elem[j][i] = A.elem[i][j];
-        }
-    }
-    
-    return(R);
-}
-
-inline v4f
-operator*(mat4x4 A, v4f P)
-{
-    v4f R = TransformVec(A, P);
-    return(R);
-};
-
-local_func mat4x4
-operator*(mat4x4 A, mat4x4 B)
-{
-    // NOTE(casey): This is written to be instructive, not optimal!
-    
-    mat4x4 R = {};
-    
-    for(int r = 0; r <= 3; ++r) // NOTE(casey): Rows (of A)
-    {
-        for(int c = 0; c <= 3; ++c) // NOTE(casey): Column (of B)
-        {
-            for(int i = 0; i <= 3; ++i) // NOTE(casey): Columns of A, rows of B!
-            {
-                R.elem[r][c] += A.elem[r][i]*B.elem[i][c];
-            }
-        }
-    }
-    
-    return(R);
-}
-
-inline mat4x4
-PerspectiveProjection(f32 aspectRatio, f32 focalLength)
-{
-    f32 a = 1.0f;
-    f32 b = aspectRatio;
-    f32 c = focalLength;
-    
-    mat4x4 r =
-    {
-        {
-            {a*c, 0, 0, 0},
-            {0, b*c, 0, 0},
-            {0,   0, 1, 0},
-            {0,   0,-1, 0}
-        }
-    };
-    
-    return r;
-};
-
-local_func mat4x4
-ColumnPicture3x3(v3f xAxis, v3f yAxis, v3f zAxis)
-{
-    mat4x4 result =
-    {
-        {
-            {xAxis.x, yAxis.x, zAxis.x, 0.0f},
-            {xAxis.y, yAxis.y, zAxis.y, 0.0f},
-            {xAxis.z, yAxis.z, zAxis.z, 0.0f},
-            {   0.0f,    0.0f,    0.0f, 1.0f}
-        }
-    };
-    
-    return result;
-};
-
-local_func mat4x4
-RowPicture3x3(v3f xAxis, v3f yAxis, v3f zAxis)
-{
-    mat4x4 result =
-    {
-        {
-            {xAxis.x, xAxis.y, xAxis.z, 0.0f},
-            {yAxis.x, yAxis.y, yAxis.z, 0.0f},
-            {zAxis.x, zAxis.y, zAxis.z, 0.0f},
-            {   0.0f,    0.0f,    0.0f, 1.0f}
-        }
-    };
-    
-    return result;
-};
-
-local_func mat4x4
-Translate(mat4x4 A, v4f T)
-{
-    mat4x4 result = A;
-    
-    result.elem[0][3] += T.x;
-    result.elem[1][3] += T.y;
-    result.elem[2][3] += T.z;
-    
-    return result;
-};
-
-local_func mat4x4
-CamTransform(v3f xAxis, v3f yAxis, v3f zAxis, v3f vecToTransform)
-{
-    mat4x4 result = RowPicture3x3(xAxis, yAxis, zAxis);
-    v4f vecToTransform_4d {vecToTransform.x, vecToTransform.y, vecToTransform.z, 1.0f};
-    result = Translate(result, -(result*vecToTransform_4d));
-    
-    return result;
-};
 
 #endif // ATOMIC_TYPES_IMPL
